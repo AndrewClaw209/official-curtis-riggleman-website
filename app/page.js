73d4725f9db6d4d071aefa83f51e5503853646fa
Andy <andyclaw209@outlook.com>
2026-09-23 14:48:22 -0700

    fix: make YouTube social icon recognizable

--- app/page.js
+++ app/page.js
@@ -105,7 +105,7 @@ function SocialIcon({ type }) {
       return (
         <svg viewBox="0 0 24 24" aria-hidden="true" className="icon-youtube">
           <rect x="3" y="6" width="18" height="12" rx="3" />
-          <path d="m10 9 5 3-5 3V9Z" />
+          <path className="youtube-play" d="m10 9 5 3-5 3V9Z" />
         </svg>
       );
     default:
