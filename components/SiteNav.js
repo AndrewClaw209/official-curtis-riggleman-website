101d1607be8db2641eec0be3e30c6e1fb1927f51
Andy <andyclaw209@outlook.com>
2026-09-23 14:45:54 -0700

    style: make shared home nav a floating icon

--- components/SiteNav.js
+++ components/SiteNav.js
@@ -7,7 +7,6 @@ export default function SiteNav() {
         <svg viewBox="0 0 24 24" aria-hidden="true">
           <path d="m3 10.8 9-7.3 9 7.3v9.2a1 1 0 0 1-1 1h-5.2v-6.4H9.2V21H4a1 1 0 0 1-1-1v-9.2Z" />
         </svg>
-        <span>Home</span>
       </Link>
     </nav>
   );
